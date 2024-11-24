f81eab40d668d4861dda62a7db69a7a278fa6215
brother-antony <elmer.antony12@gmail.com>
2024-11-23 21:14:50 -0500

    done caching

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,4 +1,10 @@
+"use client"
+
+import { unstable_noStore as noStore } from "next/cache"
+
 export default function Home() {
+  noStore()
+  
   return (
     <div>Home</div>
   )
--- src/app/reservations/create/page.tsx
+++ src/app/reservations/create/page.tsx
@@ -1,6 +1,9 @@
 import { Input } from "@nextui-org/react"
+import { unstable_noStore as noStore } from "next/cache"
 
 export default function Create() {
+    noStore()
+    
     return (
         <div className="flex w-full flex-wrap md:flex-nowrap gap-4">
             <Input type="text" label="Nombre completo" />
